65c67655baa319bd97671f411a85421a861d2737
tracy <traymarkthompson@gmail.com>
2016-07-18 23:15:18 -0500

    a few examples of controllers with parent/child an example of sibling controller added
    Will delete and continue after.

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,26 @@
  * Created by tracy on 7/17/2016.
  */
 angular.module("thToDo",[] )
+
 .controller('mainCtrl', function($scope){ /*passing name of the directive method, callback function*/
     $scope.helloWorld = function(){
         console.log("hello there. This is the helloWorld controller function in the mainCtrl!");
     };
-});
+})
+
+.controller('coolCtrl', function($scope){
+    $scope.whoAmI = function () {
+      console.log("yo! This is the coolCtrl function!");
+    };
+
+    $scope.helloWorld = function() {
+        console.log("This ain't the main ctrl");
+    }
+
+})
+
+.controller('imASibling', function ($scope) {
+    $scope.foobar = 1234;
+    //do other cool stuff
+
+    });
