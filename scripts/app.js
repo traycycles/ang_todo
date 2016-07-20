ae4b0def3e15c2d8237d67d90b004d19c306ba81
tracy <traymarkthompson@gmail.com>
2016-07-19 21:09:28 -0500

    added ng-change and ng-class for input items

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,20 @@
 /**
  * Created by tracy on 7/17/2016.
  */
-angular.module("thToDo",[] )
+angular.module("ang_todo",[] )
 
 .controller('mainCtrl', function($scope){ /*passing name of the directive method, callback function*/
-    $scope.helloWorld = function(){
-        console.log("hello there. This is the helloWorld controller function in the mainCtrl!");
-    };
-})
-
-.controller('coolCtrl', function($scope){
-    $scope.whoAmI = function () {
-      console.log("yo! This is the coolCtrl function!");
+    $scope.learningNgChange = function(){
+        console.log("An input changed");
     };
 
-    $scope.helloWorld = function() {
-        console.log("This ain't the main ctrl");
-    }
-
-})
-
-.controller('imASibling', function ($scope) {
-    $scope.foobar = 1234;
-    //do other cool stuff
+    $scope.todos = [
+        {"name": "clean the house"},
+        {"name": "walk the dog"},
+        {"name": "feed the lawn"},
+        {"name": "pay bills"},
+        {"name": "exercise"},
+        {"name": "practice piano"}
+    ]
 
     });
