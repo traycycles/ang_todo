87f06b538050917cfc0e2381fcf0412f567c4484
tracy <traymarkthompson@gmail.com>
2016-07-19 22:52:01 -0500

    used services to get (fake) data in json file using http provider (like $_GET in PHP) created mock directory for json objects

--- scripts/app.js
+++ scripts/app.js
@@ -5,27 +5,29 @@ angular.module("ang_todo",[] )
 
 .controller('mainCtrl', function($scope, dataService){ /*passing name of the directive method, callback function*/
 /*to access the service's methods, first define the service as a dependency in the controller so use as a param in the controller's callback function */
-    $scope.helloConsole = dataService.helloConsole
 
     $scope.learningNgChange = function(){
         console.log("An input changed");
     };
 
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "walk the dog"},
-        {"name": "feed the lawn"},
-        {"name": "pay bills"},
-        {"name": "exercise"},
-        {"name": "practice piano"}
-    ]
+    $scope.helloConsole = dataService.helloConsole;
+
+    dataService.getTodos(function(response){
+        console.log(response.data);
+        $scope.todos = response.data; /* todos */
+    });
 
     })
     /* 'method chaining' is chaining one method after another... totally allowed */
-.service('dataService', function () { /*passing name of service, and callback*/
+.service('dataService', function ($http) { /*passing name of service, and callback*/
 
     this.helloConsole= function(){
         console.log('This is the hello console service');
+    };
+
+    this.getTodos = function(callback){
+        $http.get('mock/todos.json')
+        .then(callback)
     }
 
 });
