1854c174c75aa30a48a1a7baad0fa358715bb149
tracy <traymarkthompson@gmail.com>
2016-07-19 21:49:45 -0500

    trying services

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,10 @@
  */
 angular.module("ang_todo",[] )
 
-.controller('mainCtrl', function($scope){ /*passing name of the directive method, callback function*/
+.controller('mainCtrl', function($scope, dataService){ /*passing name of the directive method, callback function*/
+/*to access the service's methods, first define the service as a dependency in the controller so use as a param in the controller's callback function */
+    $scope.helloConsole = dataService.helloConsole
+
     $scope.learningNgChange = function(){
         console.log("An input changed");
     };
@@ -17,4 +20,13 @@ angular.module("ang_todo",[] )
         {"name": "practice piano"}
     ]
 
-    });
+    })
+    /* 'method chaining' is chaining one method after another... totally allowed */
+.service('dataService', function () { /*passing name of service, and callback*/
+
+    this.helloConsole= function(){
+        console.log('This is the hello console service');
+    }
+
+});
+
