eceed0e1abefbcb412039d42e6ee040efea60770
tracy <traymarkthompson@gmail.com>
2016-07-19 23:40:30 -0500

    create new todo...  complete!

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,9 @@ angular.module("ang_todo",[] )
 .controller('mainCtrl', function($scope, dataService){ /*passing name of the directive method, callback function*/
 /*to access the service's methods, first define the service as a dependency in the controller so use as a param in the controller's callback function */
 
-    $scope.learningNgChange = function(){
-        console.log("An input changed");
+    $scope.addTodo = function(){
+        var todo = {name: "This is a new todo."};
+        $scope.todos.push(todo);
     };
 
     $scope.helloConsole = dataService.helloConsole;
